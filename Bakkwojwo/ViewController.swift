5d8b6768c33e372e7559b0b512a5aaddbad87a8d
YeoSeongil <seongil_yeo@naver.com>
2024-01-01 19:21:51 +0900

    Initial Commit

---
+++ Bakkwojwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bakkwojwo
+//
+//  Created by 여성일 on 2024/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
