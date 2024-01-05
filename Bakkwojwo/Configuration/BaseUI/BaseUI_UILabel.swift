b3d1479e42410dba5d91df16743d0f3f6420cbec
YeoSeongil <seongil_yeo@naver.com>
2024-01-05 21:50:12 +0900

    [CHORE] TitleLabel에서 모든 UILabel을 재사용하게 변경

--- Bakkwojwo/Configuration/BaseUI/BaseUI_UILabel.swift
+++ Bakkwojwo/Configuration/BaseUI/BaseUI_UILabel.swift
@@ -8,20 +8,21 @@
 import Foundation
 import UIKit
 
-class BaseTitleLabel: UILabel {
-    /// ViewController의 Title을 지정하는 Label을 생성합니다.
+class BaseLabel: UILabel {
+    /// ViewController의 기본이 되는 Label을 생성합니다.
     /// ```
-    /// let titleLabel = BaseTitleLabel(titleText: "Home", titleColor: .black, titleBackgroundColor: .white)
+    /// let titleLabel = BaseLabel(text: "Home", textColor: .black, backgroundColor: .white, font: h1)
     /// ```
     /// - Parameters:
     ///     - titleText: 라벨의 텍스트
     ///     - titleColor: 라벨의 텍스트 색상
     ///     - titleBackgroundColor: 라벨의 배경 색상
-    init(titleText: String, titleColor:UIColor, titleBackgroundColor: UIColor) {
+    init(text: String, textColor:UIColor, backgroundColor: UIColor, font: UIFont) {
         super.init(frame: CGRect.zero)
-        self.text = titleText
-        self.textColor = titleColor
-        self.backgroundColor = titleBackgroundColor
+        self.text = text
+        self.textColor = textColor
+        self.backgroundColor = backgroundColor
+        self.font = font
     }
     
     required init?(coder: NSCoder) {
