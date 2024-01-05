a7fa9690e57d0e537c8bdda2ab7265e3d7ff3455
YeoSeongil <seongil_yeo@naver.com>
2024-01-05 21:43:41 +0900

    [CHORE] APIManager에서 데이터를 fetch해와 kor, usd 데이터를 불러오도록 구현

--- Bakkwojwo/ViewModel/ExchangeRateViewModel.swift
+++ Bakkwojwo/ViewModel/ExchangeRateViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class ExchangeRateViewModel {
+    let repository = APIManager()
+    
+    var onUpdated: () -> Void = {}
+    
+    var exchangeRateModel: [ExchangeRateModel] = [ExchangeRateModel]()
+    {
+        didSet {
+            print("TEST : exchangeRateModel Updated!")
+            onUpdated()
+        }
+    }
+    
+    func fetchData(codes: String, onCompleted: @escaping ([ExchangeRateModel]) -> Void) {
+        repository.getExchangeRateData(codes: codes) { entity in
+            var model = [ExchangeRateModel]()
+            
+            for index in entity {
+                let item = ExchangeRateModel(country: index.country, currencyName: index.currencyName, currencyCode: index.currencyCode, basePrice: index.basePrice, changePrice: index.changePrice, signedChangeRate: index.signedChangeRate, date: index.date, time: index.time)
+                model.append(item)
+            }
+            onCompleted(model)
+        }
+    }
+    
+    func korDataLoad() {
+        self.fetchData(codes: "FRX.KRWUSD,FRX.KRWCNY,FRX.KRWJPY", onCompleted: { [weak self] model in
+            guard let self = self else { return }
+            self.exchangeRateModel = model
+            print(self.exchangeRateModel)
+        })
+    }
+
+    func usdDataLoad() {
+        self.fetchData(codes: "FRX.USDJPY,FRX.USDCNY", onCompleted: { [weak self] model in
+            guard let self = self else { return }
+            self.exchangeRateModel = model
+        })
+    }
+
+}
