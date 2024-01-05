02ad7886aa9debff1038b72b761741a2ca423431
YeoSeongil <seongil_yeo@naver.com>
2024-01-05 21:41:53 +0900

    [ADD] ExchangeRateViewModel 생성, [CHORE] APIManager에서 데이터를 fetch해와 kor데이터와 usd데이터를 불러오도록 구현

---
+++ Bakkwojwo/API/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  Bakkwojwo
+//
+//  Created by 여성일 on 1/5/24.
+//
+
+import Foundation
---
+++ Bakkwojwo/View/ExchangeRateSectionCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExchangeRateSectionCollectionViewCell.swift
+//  Bakkwojwo
+//
+//  Created by 여성일 on 1/5/24.
+//
+
+import UIKit
+
+class ExchangeRateSectionCollectionViewCell: UICollectionViewCell {
+    
+}
