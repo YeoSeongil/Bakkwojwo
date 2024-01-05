04caff1f69dd225c4414095ffb622fd8651b94f6
YeoSeongil <seongil_yeo@naver.com>
2024-01-05 21:46:56 +0900

    [ADD] Entity와 구분하기 위해 ExchangeRateModel 생성, [CHORE] 기본 Model 구현

--- Bakkwojwo/Model/ExchangeRateModel.swift
+++ Bakkwojwo/Model/ExchangeRateModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct ExchangeRateModel: Codable {
+    let country: String // 미국
+    let currencyName: String // 달러
+    let currencyCode: String // USD
+    let basePrice: Double // 1301.1
+    let changePrice: Double // 13
+    let signedChangeRate: Double // - 0.00112
+    let date: String
+    let time: String
+}
