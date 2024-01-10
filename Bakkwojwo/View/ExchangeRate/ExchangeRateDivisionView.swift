31174fafa273413296f919777d3cef158d7bb062
YeoSeongil <seongil_yeo@naver.com>
2024-01-10 15:18:20 +0900

    [ADD] ExchangeRateDivisionView 추가

---
+++ Bakkwojwo/View/ExchangeRate/ExchangeRateDivisionView.swift
@@ -0,0 +1,20 @@
+//
+//  ExchangeRateDivisionView.swift
+//  Bakkwojwo
+//
+//  Created by 여성일 on 1/10/24.
+//
+
+import UIKit
+
+class ExchangeRateDivisionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
