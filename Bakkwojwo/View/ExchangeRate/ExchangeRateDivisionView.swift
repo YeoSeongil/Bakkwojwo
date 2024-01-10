2197e9777cf1ecd137f96504af60d23b7e1e2f32
YeoSeongil <seongil_yeo@naver.com>
2024-01-10 15:30:45 +0900

    [CHORE] 구분 View 구현

--- Bakkwojwo/View/ExchangeRate/ExchangeRateDivisionView.swift
+++ Bakkwojwo/View/ExchangeRate/ExchangeRateDivisionView.swift
@@ -6,15 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ExchangeRateDivisionView: UIView {
+    let DivisionCurrencyLable: BaseLabel = {
+        let label = BaseLabel(text: "통화명", textColor: .black, backgroundColor: .clear, font: .h4!)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let DivisionBasePriceLabel: BaseLabel = {
+        let label = BaseLabel(text: "매매기준가", textColor: .black , backgroundColor: .white, font: .h4!)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let DivisionChangePriceLabel: BaseLabel = {
+        let label = BaseLabel(text: "환율변동", textColor: .black , backgroundColor: .white, font: .h4!)
+        label.textAlignment = .center
+        return label
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setView()
+        self.setAutoLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setView() {
+        [self.DivisionCurrencyLable, self.DivisionBasePriceLabel, self.DivisionChangePriceLabel].forEach { self.addSubview($0) }
+    }
+    
+    func setAutoLayout() {
+        self.DivisionCurrencyLable.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-144)
+        }
+        
+        self.DivisionBasePriceLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(self.DivisionCurrencyLable.snp.trailing)
+            $0.trailing.equalToSuperview().offset(-72)
+        }
+        
+        self.DivisionChangePriceLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(self.DivisionBasePriceLabel.snp.trailing).offset(10)
+            $0.trailing.equalToSuperview()
+        }
+    }
 }
