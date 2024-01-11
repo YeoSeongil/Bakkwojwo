a693963c2b30a6da30cb0b4bc0cb9110b8fc4f7c
YeoSeongil <seongil_yeo@naver.com>
2024-01-11 19:43:41 +0900

    [CHORE] Realm Model 구현 (MyCurrencyModel)

--- Bakkwojwo/Model/MyCurrencyModel.swift
+++ Bakkwojwo/Model/MyCurrencyModel.swift
@@ -8,6 +8,12 @@
 import Foundation
 import RealmSwift
 
-class MyCurrency: Object {
-    @Persisted 
+class MyCurrencyModel: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var currencyCode: String
+    
+    convenience init(currencyCode: String) {
+        self.init()
+        self.currencyCode = currencyCode
+    }
 }
