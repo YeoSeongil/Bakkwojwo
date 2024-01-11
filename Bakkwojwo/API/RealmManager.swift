eb7ca48c882d956cb5bfde28d527297b2a7e500c
YeoSeongil <seongil_yeo@naver.com>
2024-01-11 19:32:58 +0900

    [CHORE] RealmManager 싱글톤으로 구현

--- Bakkwojwo/API/RealmManager.swift
+++ Bakkwojwo/API/RealmManager.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+import RealmSwift
+
+protocol BakkwojwoRealm {
+    func read<T: Object>(_ object: T.Type) -> Results<T>
+    func write<T: Object>(_ object: T)
+    func update<T: Object>(_ object:T, completion: @escaping ((T)->()))
+    func delete<T: Object>(_ object: T)
+}
+
+final class RealmManager: BakkwojwoRealm {
+
+    static let shared = RealmManager()
+    
+    private let realmInstance: Realm
+    
+    private init() {
+        self.realmInstance = try! Realm()
+    }
+    
+    func getLocalRealmURL() {
+        print("Realm URL:", realmInstance.configuration.fileURL!)
+    }
+    
+    func read<T: Object>(_ object: T.Type) -> Results<T> {
+        return realmInstance.objects(object)
+    }
+    
+    func update<T>(_ object: T, completion: @escaping ((T) -> ())) {
+        do {
+            try realmInstance.write {
+                completion(object)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func write<T: Object>(_ object: T) {
+        do {
+            try realmInstance.write {
+                realmInstance.add(object)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func delete<T: Object>(_ object: T) {
+        do {
+            try realmInstance.write {
+                realmInstance.delete(object)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    
+}
