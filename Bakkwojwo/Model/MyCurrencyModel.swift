56c9773174e05573c755a60f5a3691dfade7f4c2
YeoSeongil <seongil_yeo@naver.com>
2024-01-13 21:09:56 +0900

    [CHORE] 초기 모델 초기화를 위한 item 배열 추가

--- Bakkwojwo/Model/MyCurrencyModel.swift
+++ Bakkwojwo/Model/MyCurrencyModel.swift
@@ -16,4 +16,7 @@ class MyCurrencyModel: Object {
         self.init()
         self.currencyCode = currencyCode
     }
+    
+    let item = ["KRW", "USD", "JPY", "CNY", "EUR", "GBP", "RUB", "HKD", "THB", "TWD", "PHP", "SGD", "VND", "MYR", "MNT", "BHD", "BDT", "BND", "SAR", "LKR", "AED", "OMR", "JOD", "ILS", "INR", "IDR", "QAR", "KZT", "KWD", "PKR", "AUD", "NZD", "CAD", "MXN", "BRL", "CLP", "COP", "ZAR", "NOK", "DKK", "SEK", "CHF", "CZK", "TRY", "PLN", "HUF", "DZD", "EGP", "KES", "TZS"]
 }
+
