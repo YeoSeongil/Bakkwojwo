22423166893957582c3232775f641c2cbef2d2cc
YeoSeongil <seongil_yeo@naver.com>
2024-01-14 19:28:05 +0900

    [CHORE] 버튼의 체크 상태를 담고 있을 isCheck 추가

--- Bakkwojwo/Model/MyCurrencyModel.swift
+++ Bakkwojwo/Model/MyCurrencyModel.swift
@@ -11,10 +11,12 @@ import RealmSwift
 class MyCurrencyModel: Object {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var currencyCode: String
+    @Persisted var isCheck: Bool
     
-    convenience init(currencyCode: String) {
+    convenience init(currencyCode: String, isCheck: Bool) {
         self.init()
         self.currencyCode = currencyCode
+        self.isCheck = isCheck
     }
     
     let item = ["KRW", "USD", "JPY", "CNY", "EUR", "GBP", "RUB", "HKD", "THB", "TWD", "PHP", "SGD", "VND", "MYR", "MNT", "BHD", "BDT", "BND", "SAR", "LKR", "AED", "OMR", "JOD", "ILS", "INR", "IDR", "QAR", "KZT", "KWD", "PKR", "AUD", "NZD", "CAD", "MXN", "BRL", "CLP", "COP", "ZAR", "NOK", "DKK", "SEK", "CHF", "CZK", "TRY", "PLN", "HUF", "DZD", "EGP", "KES", "TZS"]
