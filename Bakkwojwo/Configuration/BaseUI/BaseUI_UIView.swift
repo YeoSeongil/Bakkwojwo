592eaa31847fdf60aa33a3ff6322d49d9a5a7a3b
YeoSeongil <seongil_yeo@naver.com>
2024-01-16 18:01:26 +0900

    [CHORE] BaseView 구현

--- Bakkwojwo/Configuration/BaseUI/BaseUI_UIView.swift
+++ Bakkwojwo/Configuration/BaseUI/BaseUI_UIView.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setView()
+        self.setAutoLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    /// 해당 View를 셋업 합니다. (배경색상, UI 프로퍼티 할당 등)
+    /// ```
+    /// func setView() {
+    ///     self.view.addSubview(label)
+    ///     self.view.backgroundColor = .blue
+    /// }
+    func setView() {
+    }
+    
+    /// 해당 View UI요소의  AutoLayout(제약조건)을 셋업합니다.
+    /// ```
+    /// func setAutoLayout() {
+    ///      self.label.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
+    /// }
+    func setAutoLayout() {
+        
+    }
+}
