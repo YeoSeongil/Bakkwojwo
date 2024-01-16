077940d83515cf380e06781f47f5f4bc8f1d4e03
YeoSeongil <seongil_yeo@naver.com>
2024-01-16 19:18:28 +0900

    [CHORE] 계산기의 키를 Custom UI로 구현

--- Bakkwojwo/Configuration/CustomUI/Custom_CalculatorKey.swift
+++ Bakkwojwo/Configuration/CustomUI/Custom_CalculatorKey.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+class CalculatorKey: UIButton {
+    
+    init(keyTitle: String) {
+        super.init(frame: CGRect.zero)
+        self.setTitle(keyTitle, for: .normal)
+        self.setTitleColor(.black, for: .normal)
+        self.backgroundColor = .clear
+        self.titleLabel?.font = .h5!
+        self.addTarget(self, action: #selector(keyTapped), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func keyTapped(_ sender: UIButton) {
+        guard let symbol = sender.currentTitle else { return }
+        print(symbol)
+    }
+}
