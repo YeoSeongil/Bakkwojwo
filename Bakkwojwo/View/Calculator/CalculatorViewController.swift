00fe3f89817c2cd30762dbc1df4f0ce39667a68c
YeoSeongil <seongil_yeo@naver.com>
2024-01-17 21:07:57 +0900

    [CHORE] Delegate 패턴으로 위임받아 inputView를 업데이트 하도록 구현

--- Bakkwojwo/View/Calculator/CalculatorViewController.swift
+++ Bakkwojwo/View/Calculator/CalculatorViewController.swift
@@ -10,30 +10,37 @@ import SnapKit
 
 class CalculatorViewController: BaseViewController {
 
-    let testView = CalculatorView()
+    let CalculatorKey = CalculatorView()
+    let CalculatorResult  = CalculatorResultView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(UIScreen.main.bounds.size.height/2.3)
+        self.CalculatorKey.delegate = self
     }
 
-    
     override func setView() {
         self.view.backgroundColor = .white
-        self.view.addSubview(self.testView)
-        
+        [self.CalculatorKey, self.CalculatorResult].forEach { self.view.addSubview($0)}
     }
     
     override func setAutoLayout() {
-        self.testView.snp.makeConstraints {
+        self.CalculatorKey.snp.makeConstraints {
             $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
             $0.width.equalTo(UIScreen.main.bounds.size.width)
             $0.height.equalTo(UIScreen.main.bounds.size.height / 3.0)
+            
+            self.CalculatorResult.snp.makeConstraints {
+                $0.width.equalTo(UIScreen.main.bounds.size.width)
+                $0.height.equalTo(UIScreen.main.bounds.size.height / 13.0)
+                $0.bottom.equalTo(self.CalculatorKey.snp.top)
+            }
         }
     }
-    
-    
+}
 
-
-
+extension CalculatorViewController: CalculatorViewDelegate {
+    func onUpdateInputView(_ k: String) {
+        self.CalculatorResult.inputOperatorView.text = k
+    }
 }
