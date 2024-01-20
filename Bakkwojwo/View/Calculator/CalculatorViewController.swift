c4f7401d1d532a0a28b1f84f4e8160bdb85882d0
YeoSeongil <seongil_yeo@naver.com>
2024-01-20 21:23:00 +0900

    [CHORE] 계산 기능 구현 > 로직 수정 필요 (너무 복잡? 하드 코딩 느낌)

--- Bakkwojwo/View/Calculator/CalculatorViewController.swift
+++ Bakkwojwo/View/Calculator/CalculatorViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 import SnapKit
 
 class CalculatorViewController: BaseViewController {
-
+    
+    private var operArr = [String]()
     let CalculatorKey = CalculatorView()
     let CalculatorResult  = CalculatorResultView()
     
@@ -18,7 +19,7 @@ class CalculatorViewController: BaseViewController {
         print(UIScreen.main.bounds.size.height/2.3)
         self.CalculatorKey.delegate = self
     }
-
+    
     override func setView() {
         self.view.backgroundColor = .white
         [self.CalculatorKey, self.CalculatorResult].forEach { self.view.addSubview($0)}
@@ -40,7 +41,66 @@ class CalculatorViewController: BaseViewController {
 }
 
 extension CalculatorViewController: CalculatorViewDelegate {
-    func onUpdateInputView(_ k: String) {
-        self.CalculatorResult.inputOperatorView.text = k
+    func calculator(_ k: String)  {
+        //        // inPutOperatorView의 Text가 0이고, 입력하는
+        //        if k != "DEL" && self.CalculatorResult.inputOperatorView.text?.first == "0"{
+        //            self.CalculatorResult.inputOperatorView.text?.removeFirst()
+        //        }
+        
+        print(self.CalculatorResult.inputOperatorView.text)
+        // 큰 if -> 숫자 패드를 눌렀을 때 0을 지우고 k로 이어감.
+        if k != "+" && k != "-" && k != "÷" && k != "×" && k != "DEL" && k != "." {
+            if self.CalculatorResult.inputOperatorView.text?.first == "0" && self.CalculatorResult.inputOperatorView.text?.count == 1 {
+                self.CalculatorResult.inputOperatorView.text?.removeFirst()
+            }
+            self.CalculatorResult.inputOperatorView.text = self.CalculatorResult.inputOperatorView.text! + k
+        }  else if k == "DEL" { // 큰 DEL 눌렀을 때는 예외처리
+            if self.CalculatorResult.inputOperatorView.text?.last == " " {
+                self.CalculatorResult.inputOperatorView.text = String(self.CalculatorResult.inputOperatorView.text!.dropLast(3))
+            }  else if  self.CalculatorResult.inputOperatorView.text?.count == 1 {
+                self.CalculatorResult.inputOperatorView.text = "0"
+            } else  {
+                self.CalculatorResult.inputOperatorView.text = String(self.CalculatorResult.inputOperatorView.text!.dropLast(1))
+            }
+        } else if k == "." {
+            if self.CalculatorResult.inputOperatorView.text?.last != "." &&   self.CalculatorResult.inputOperatorView.text?.last != " " && self.operArr.last != "."{
+                self.CalculatorResult.inputOperatorView.text = self.CalculatorResult.inputOperatorView.text! + k
+                self.operArr.append(k)
+            }
+        }
+        else {
+            if self.CalculatorResult.inputOperatorView.text?.last != " " &&  self.CalculatorResult.inputOperatorView.text?.last != "."{
+                self.CalculatorResult.inputOperatorView.text = self.CalculatorResult.inputOperatorView.text! + " " + k + " "
+                self.operArr.append(k)
+            }
+        }
+        
+        guard let inputViewText: String = self.CalculatorResult.inputOperatorView.text else { return }
+        
+        let calculateValueTexts: [String] = inputViewText.split(separator: " ").map {String($0)}
+        var result = Double(calculateValueTexts[0])!
+        print(calculateValueTexts)
+        print(operArr)
+        var tempOperation = ""
+        for i in 1..<calculateValueTexts.count {
+            if calculateValueTexts[i] != "+" && calculateValueTexts[i] != "-" && calculateValueTexts[i] != "÷" && calculateValueTexts[i] != "×" {
+                switch tempOperation {
+                case "+":
+                    result = result + Double(calculateValueTexts[i])!
+                case "-":
+                    result = result - Double(calculateValueTexts[i])!
+                case "×":
+                    result = result * Double(calculateValueTexts[i])!
+                case "÷":
+                    result = result / Double(calculateValueTexts[i])!
+                default:
+                    break
+                }
+            } else {
+                tempOperation = calculateValueTexts[i]
+            }
+        }
+        self.CalculatorResult.resultView.text = String(result)
+        print(result)
     }
 }
