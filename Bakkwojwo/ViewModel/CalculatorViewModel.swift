7e1add7d27c4f898d66fe9d87b875c380965592b
YeoSeongil <seongil_yeo@naver.com>
2024-01-21 15:04:26 +0900

    [CHORE] 기존 계산 기능 ViewModel에서 구현하여 View에서 Bind하도록 구현 : 기존 CalculatorViewController에서 Delegate 받아 구현하는 방법은 ViewController의 부담이 커지고, ResultView 객체의 InputView Text에 직접 접근하여 요소를 수정하는 방법은 의존성을 증가시킨다고 생각했기에 ViewModel에서 구현

--- Bakkwojwo/ViewModel/CalculatorViewModel.swift
+++ Bakkwojwo/ViewModel/CalculatorViewModel.swift
@@ -6,3 +6,75 @@
 //
 
 import Foundation
+
+enum Operator: String {
+    case plus = "+"
+    case minus = "-"
+    case multiply = "×"
+    case divide = "÷"
+    case dot = "."
+    case delete = "DEL"
+}
+
+class CalculatorViewModel {
+    var onUpdated: () -> Void = {}
+    var operArr = [String]()
+    var currentInputViewText: String = "0"
+    {
+        didSet {
+            onUpdated()
+            print("on Updated!")
+        }
+    }
+    
+    func calculate(_ k: String) {
+        if k != Operator.plus.rawValue && k != Operator.minus.rawValue && k != Operator.multiply.rawValue && k != Operator.divide.rawValue && k != Operator.delete.rawValue && k != Operator.dot.rawValue {
+            if self.currentInputViewText.first == "0" && self.currentInputViewText.count == 1 {
+                self.currentInputViewText.removeFirst()
+            }
+            self.currentInputViewText = self.currentInputViewText + k
+        }  else if k == "DEL" {
+            if self.currentInputViewText.last == " " {
+                self.currentInputViewText = String(self.currentInputViewText.dropLast(3))
+            }  else if  self.currentInputViewText.count == 1 {
+                self.currentInputViewText = "0"
+            } else  {
+                self.currentInputViewText = String(self.currentInputViewText.dropLast(1))
+            }
+        } else if k == "." {
+            if self.currentInputViewText.last != "." &&   self.currentInputViewText.last != " " && self.operArr.last != "."{
+                self.currentInputViewText = self.currentInputViewText + k
+                self.operArr.append(k)
+            }
+        }
+        else {
+            if self.currentInputViewText.last != " " &&  self.currentInputViewText.last != "."{
+                self.currentInputViewText = self.currentInputViewText + " " + k + " "
+                self.operArr.append(k)
+            }
+        }
+        
+        let calculateValueTexts = self.currentInputViewText.split(separator: " ").map { String($0)}
+        var result = Double(calculateValueTexts[0])!
+        var temp = " "
+        for i in 1..<calculateValueTexts.count {
+            if calculateValueTexts[i] != "+" && calculateValueTexts[i] != "-" && calculateValueTexts[i] != "÷" && calculateValueTexts[i] != "×" {
+                switch temp {
+                case "+":
+                    result = result + Double(calculateValueTexts[i])!
+                case "-":
+                    result = result - Double(calculateValueTexts[i])!
+                case "×":
+                    result = result * Double(calculateValueTexts[i])!
+                case "÷":
+                    result = result / Double(calculateValueTexts[i])!
+                default:
+                    break
+                }
+            } else {
+                temp = calculateValueTexts[i]
+            }
+        }
+    }
+}
+
