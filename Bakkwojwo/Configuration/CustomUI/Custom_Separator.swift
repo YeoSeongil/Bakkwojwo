79c68f18014a7adbd7680d09c8f1373d746f0793
YeoSeongil <seongil_yeo@naver.com>
2024-01-21 17:15:36 +0900

    [CHORE] 커스텀 구분선 구현

--- Bakkwojwo/Configuration/CustomUI/Custom_Separator.swift
+++ Bakkwojwo/Configuration/CustomUI/Custom_Separator.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class Custom_Separator: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setView()
+        self.setAutoLayout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setView() {
+        self.backgroundColor = .clear
+        self.tintColor = .black
+        self.layer.borderWidth = 1
+    }
+    
+    func setAutoLayout() {
+        self.snp.makeConstraints {
+            $0.width.equalTo(300)
+            $0.height.equalTo(1)
+        }
+    }
 
 }
