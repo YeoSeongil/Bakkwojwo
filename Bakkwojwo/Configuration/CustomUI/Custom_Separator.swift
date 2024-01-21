eeea5cfe94bf70dfc2c5074307413e41527423d2
YeoSeongil <seongil_yeo@naver.com>
2024-01-21 17:15:27 +0900

    [CHORE] 커스텀 구분선 생성

---
+++ Bakkwojwo/Configuration/CustomUI/Custom_Separator.swift
@@ -0,0 +1,20 @@
+//
+//  Custom_Separator.swift
+//  Bakkwojwo
+//
+//  Created by 여성일 on 1/21/24.
+//
+
+import UIKit
+
+class Custom_Separator: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
