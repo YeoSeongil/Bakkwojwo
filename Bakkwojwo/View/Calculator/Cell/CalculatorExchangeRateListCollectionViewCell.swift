1cf23b5dfca49cb7aebf775a7dd38b88e32f0915
YeoSeongil <seongil_yeo@naver.com>
2024-01-22 21:45:13 +0900

    [ADD] 환율 계산 결과를 나타낼 Cell 생성

---
+++ Bakkwojwo/View/Calculator/Cell/CalculatorExchangeRateListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorExchangeRateListCollectionViewCell.swift
+//  Bakkwojwo
+//
+//  Created by 여성일 on 1/22/24.
+//
+
+import UIKit
+
+class CalculatorExchangeRateListCollectionViewCell: UICollectionViewCell {
+    
+}
